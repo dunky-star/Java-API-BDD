372e5a409f3cc14272cdc26d5efbf1f3f46975a0
dunky-star <dunkygeoffrey39@gmail.com>
2024-09-26 13:29:28 -0500

    Java API Behavior-Driven Development Testing

---
+++ Unit-Test-Basics/src/test/java/com/dunky/CalculatorTest.java
@@ -0,0 +1,16 @@
+package com.dunky;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    public void addTest(){
+        Calculator calc = new Calculator();
+        int actualResult = calc.add(10,2);
+        assertEquals(30, actualResult);
+
+    }
+}
