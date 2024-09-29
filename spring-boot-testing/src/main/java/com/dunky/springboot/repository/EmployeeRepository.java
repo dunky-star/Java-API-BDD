2fddd9fa27b61ec6d230e386e512c26658e8573c
dunky-star <dunkygeoffrey39@gmail.com>
2024-09-28 23:48:57 -0500

    Unit testing repository layer

--- spring-boot-testing/src/main/java/com/dunky/springboot/repository/EmployeeRepository.java
+++ spring-boot-testing/src/main/java/com/dunky/springboot/repository/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.dunky.springboot.repository;
 
 import com.dunky.springboot.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -9,4 +11,9 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     // Custom JPA Query to find employee by email
     Optional<Employee> findByEmail(String email);
+
+    // Define custom query using Native SQL with named params
+    @Query(value = "SELECT * FROM employee e WHERE e.first_name =: firstName AND e.last_name =: lastname",
+    nativeQuery = true)
+    Employee findByNativeQueryNamed(@Param("firstName") String firstName, @Param("lastName") String lastName);
 }
