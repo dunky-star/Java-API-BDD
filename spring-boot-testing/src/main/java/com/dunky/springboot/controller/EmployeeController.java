55a652c8a4b9dd2809e6dd00179d446afc1797f0
dunky-star <dunkygeoffrey39@gmail.com>
2024-10-03 22:37:31 -0500

    Created Unit test for API endpoint of updateEmployee and deleteEmployee

--- spring-boot-testing/src/main/java/com/dunky/springboot/controller/EmployeeController.java
+++ spring-boot-testing/src/main/java/com/dunky/springboot/controller/EmployeeController.java
@@ -37,4 +37,31 @@ public class EmployeeController {
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @PutMapping("{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable("id") long employeeId,
+                                                   @RequestBody Employee employee){
+        return employeeService.getEmployeeById(employeeId)
+                .map(savedEmployee -> {
+
+                    savedEmployee.setFirstName(employee.getFirstName());
+                    savedEmployee.setLastName(employee.getLastName());
+                    savedEmployee.setEmail(employee.getEmail());
+
+                    Employee updatedEmployee = employeeService.updateEmployee(savedEmployee);
+                    return new ResponseEntity<>(updatedEmployee, HttpStatus.OK);
+
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable("id") long employeeId){
+
+        employeeService.deleteEmployee(employeeId);
+
+        return new ResponseEntity<String>("Employee deleted successfully!.", HttpStatus.OK);
+
+    }
+
+
 }
